3869db2d3afa62d72647198d9dfd85efbd0b2021
janajohny <56085771+janajohny@users.noreply.github.com>
2021-11-29 11:56:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = var.cluster_name
-  kubernetes_version  = var.kubernetes_version
+  #kubernetes_version  = var.kubernetes_version
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = var.cluster_name
