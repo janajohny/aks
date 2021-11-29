8671f48e453937bf2d4489f0991b4626f1dc42eb
janajohny <56085771+janajohny@users.noreply.github.com>
2021-11-29 11:08:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,11 @@
 provider "azurerm" {
+      features {}
      client_id       =   var.client_id
      client_secret   =   var.client_secret
      subscription_id =   var.subscription_id
      tenant_id       =   var.tenant_id
     
-     features {}
+    
  }
 
 resource "azurerm_resource_group" "rg" {
